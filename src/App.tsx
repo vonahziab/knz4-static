11b1d2d595bddb230878a56092a69d539ce63c01
Arman <baizhanov.arman@icloud.com>
2024-09-23 06:04:15 +0500

    build

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,56 @@ import { FC } from 'react';
 
 const App: FC = () => {
 	return (
-		<div
-			style={{
-				height: '100%',
-				overflow: 'scroll',
-				position: 'absolute',
-				bottom: '0',
-				right: '0',
-				top: '0',
-				left: '0',
-				backgroundColor: 'black',
-				color: 'white',
-			}}
-		>
-			<div style={{ fontSize: 20, textAlign: 'center' }}>Hello, Koinz v0.10</div>
+		<div>
 			<div
 				style={{
-					height: 24,
-					width: '100%',
-					backgroundColor: 'green',
-				}}
-			/>
+					height: '100%',
+					overflow: 'scroll',
+					position: 'absolute',
+					bottom: '0',
+					right: '0',
+					top: '0',
+					left: '0',
 
-			<div
-				style={{
-					height: 'var(--safe-area-inset-bottom)',
-					width: '100%',
-					backgroundColor: 'orange',
+					color: 'white',
+					backgroundColor: 'rgba(0, 0, 0, 0.8)',
+					padding: '16px 16px 0px',
+					paddingBottom: 'calc(var(safe-area-inset-bottom) + 16px)',
+					boxSizing: 'border-box',
 				}}
-			/>
+			>
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						gap: 16,
+						boxSizing: 'border-box',
+					}}
+				>
+					<div style={{ fontSize: 20, textAlign: 'center' }}>Hello, Koinz v0.11</div>
+					<div
+						style={{
+							height: 24,
+							width: '100%',
+							backgroundColor: 'green',
+						}}
+					/>
 
+					<div
+						style={{
+							height: 'var(--safe-area-inset-bottom)',
+							width: '100%',
+							backgroundColor: 'orange',
+						}}
+					/>
+				</div>
+			</div>
 			<div
 				style={{
 					height: 100,
 					width: '100vw',
 					position: 'fixed',
+					left: 0,
 					bottom: 0,
 					paddingBottom: 'var(--safe-area-inset-bottom)',
 				}}
@@ -44,7 +59,7 @@ const App: FC = () => {
 				<div
 					style={{
 						height: 100,
-						width: '100vw',
+						width: '100%',
 						display: 'flex',
 						justifyContent: 'center',
 						alignItems: 'center',
