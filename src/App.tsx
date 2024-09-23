da3e039f8f4221f14503d0902b3a90f8b9fd4155
Arman <baizhanov.arman@icloud.com>
2024-09-23 05:19:13 +0500

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { FC } from 'react';
 
 const App: FC = () => {
-	return <>Hello, World! Koinz v4 :)</>;
+	return (
+		<>
+			Hello, World! Koinz v0.1 :)
+			<div
+				style={{
+					height: 24,
+					width: '100%',
+					backgroundColor: 'green',
+				}}
+			/>
+			<div
+				style={{
+					height: 'var(--safe-area-inset-bottom)',
+					width: '100%',
+					backgroundColor: 'red',
+				}}
+			/>
+		</>
+	);
 };
 
 export default App;
